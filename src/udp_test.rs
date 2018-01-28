45d5036ceb5139ed83c38ff091a9933fbc9b8bab
Michael Wilson <wilsoniya@gmail.com>
2018-01-28 12:04:30 -0800

    wip

--- src/udp_test.rs
+++ src/udp_test.rs
@@ -2,20 +2,18 @@
 extern crate log;
 extern crate env_logger;
 extern crate clap;
-
 extern crate futures;
 extern crate tokio_core;
 
+use std::io::{Error, ErrorKind};
 use std::io::Result;
 use std::net::SocketAddr;
 use std::str::from_utf8;
 
-use futures::Future;
 use futures::Sink;
 use futures::stream::Stream;
 use tokio_core::net::{UdpCodec, UdpSocket};
 use tokio_core::reactor::Core;
-
 
 struct MyCodec;
 
@@ -24,11 +22,15 @@ impl UdpCodec for MyCodec {
     type Out = (SocketAddr, String);
 
     fn decode(&mut self, src: &SocketAddr, buf: &[u8]) -> Result<Self::In> {
-        let incoming = from_utf8(buf).expect("could not decode incoming datagram");
-        Ok((*src, incoming.to_owned()))
+        from_utf8(buf).and_then(|incoming| {
+            info!("rx: {}", incoming.trim());
+            Ok((*src, incoming.to_owned()))
+        }).or_else(|err| {
+            Err(Error::new(ErrorKind::InvalidData, err))
+        })
     }
     fn encode(&mut self, (addr, msg): Self::Out, buf: &mut Vec<u8>) -> SocketAddr {
-        let new_msg = format!("received: {}", msg);
+        let new_msg = format!("echo: {}", msg);
         buf.extend_from_slice(new_msg.as_bytes());
         addr
     }
@@ -36,8 +38,6 @@ impl UdpCodec for MyCodec {
 
 
 fn main() {
-    println!("hello, world");
-
     let mut core = Core::new().expect("Could not create reactor core");
     let handle = core.handle();
 
@@ -47,13 +47,7 @@ fn main() {
     let framed: tokio_core::net::UdpFramed<MyCodec> = socket.framed(codec);
 
     let (sink, stream) = framed.split();
-
-    let out_stream = stream.map(|(addr, msg)| {
-        println!("received: {} from {}", msg, addr);
-        (addr, msg)
-    });
-
-    let sender = sink.send_all(out_stream);
+    let sender = sink.send_all(stream);
 
     core.run(sender);
 }
